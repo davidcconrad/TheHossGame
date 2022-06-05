551a53fe915c4c81114a6e12d691320a9afc8946
David Chaparro <davidacm7@outlook.com>
2022-06-04 20:34:09 -0500

    Added Rename-Solution script

---
+++ Rename-Solution.ps1
@@ -0,0 +1,48 @@
+Param(
+    $NewName
+)
+
+$ExcludedPaths = "bin*|obj*|Rename-Solution*"
+$CurrentName = "TheHossGame"
+
+function Get-DirectoriesStartingWith($Start) {
+    Get-ChildItem -Recurse -Filter ($Start + '**')
+    | Where-Object {
+        $_.FullName -notmatch $ExcludedPaths
+    } 
+}
+
+function Get-AllChildItems() {
+    return Get-ChildItem -Recurse -File
+}
+
+function Rename-Items {
+    param(
+        [Parameter(ValueFromPipeline = $true)]
+        $File
+    )
+    Begin {}
+    Process {
+        Rename-Item $File $File.Name.Replace($CurrentName, $NewName)
+    }
+    End {}
+}
+
+function Update-FilesContent {
+    param (
+        [Parameter(ValueFromPipeline = $true)]
+        $File
+    )
+    Begin {}
+    Process {
+        Write-Host $_.FullName
+        $Content = Get-Content $_
+        $NewContent = $Content.Replace("TheHossGame", $NewName)
+        Set-Content -Path $_ $NewContent
+    }
+    End {}
+}
+
+Get-DirectoriesStartingWith -Start $CurrentName | Rename-Items
+Get-AllChildItems | Update-FilesContent
+
